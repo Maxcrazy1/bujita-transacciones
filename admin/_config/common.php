3581c0a9eb547470950d2d9296d5c594c56f228c
SCristian09 <cserrano@octa.dev>
2023-10-20 11:21:30 -0400

    feat: replace phone by telefono

--- admin/_config/common.php
+++ admin/_config/common.php
@@ -97,11 +97,11 @@ $header_logo .= '<body><div class="preheader" style="color: transparent; display
 													<tr style="background:#000000;">
 														<td align="left" style="font-family:\'Montserrat\',Helvetica,Roboto,Arial,sans-serif; width:25%;padding-top:10px;padding-bottom:10px;padding-left:20px;padding-right:0px">
 															<a style="border:none;"href="'.SITE_URL.'" target="_blank">'.
-															   '<img src="'.SITE_URL.'images/logo.png" width="auto" height="50" alt=" Logo" class="CToWUd">
+															   '<img src="'.SITE_URL.'images/logo.png" width="auto" height="50" alt="" class="CToWUd">
 															</a>
 														</td>
 														<td align="right" style="font-family:\'Montserrat\',Helvetica,Roboto,Arial,sans-serif; width:65%;padding-top:10px;padding-bottom:10px;padding-left:0px;padding-right:20px;font-size:17px;color:#f1bb00">
-														<strong>PHONE</strong><br><a style="color:#fff;text-decoration:none" href="tel:+1-904-310-0080" target="_blank">'.$company_phone.'</a>
+														<strong>Telefono:</strong><br><a style="color:#fff;text-decoration:none" href="tel:'.$company_phone.'" target="_blank">'.$company_phone.'</a>
 														</td>
 													</tr>
 												</tbody>
